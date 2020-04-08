89b2b6833421c7cf38f31ad41ecc08384744c9b3
Dmitriy265 <63237845+Dmitriy265@users.noreply.github.com>
2020-04-08 13:11:46 +0300

    Add files via upload

---
+++ bootstrap/index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>МОЁ ПОРТФОЛИО</title>
+  </head>
+  <body style="background-color:Pink">>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <h2 class="text-center mt-5">Oбо мне</h2>
+    <hr class="mb-0">
+    <p class="bg-info text-white p-3 text-justify border border-dark "><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, mollitia eius necessitatibus quasi dolor veritatis dignissimos dolorem consequatur fugiat earum inventore eligendi accusamus repudiandae magni odit cupiditate modi voluptates voluptate.</span>
+    <span>Consequuntur, expedita, harum id impedit hic totam ratione nobis maiores. Tenetur, nemo, adipisci, expedita, commodi sit consequuntur eligendi dicta illum blanditiis soluta autem tempore eveniet culpa enim reiciendis necessitatibus aspernatur?</span>
+    <span>Sed aliquid temporibus facere aspernatur error repudiandae animi? Aperiam, earum, perferendis, reiciendis consequuntur atque placeat ex tenetur delectus sunt provident quas accusantium error et ut autem amet obcaecati praesentium fugiat.</span></p>
+     <h2 class="text-center mt-5">ПОРТФОЛИО</h2>
+     <div id="carouselExampleIndicators" class="carousel slide my-4  w-50 mx-auto" data-ride="carousel">
+      <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+      
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__340.jpg" width="450" height="350" alt="500">
+    </div>
+    <div class="carousel-item">
+      <img src="https://st.depositphotos.com/1025323/2317/i/450/depositphotos_23176146-stock-photo-cool-fractal-paint.jpg" width="700" height="350" class="d-block 300" alt="...">
+      </div>
+    <div class="carousel-item">
+      <img src="https://st.depositphotos.com/1238677/3560/i/450/depositphotos_35601317-stock-photo-spiral-galaxy-milky-way.jpg" width="700" height="350" class="d-block 300" alt="...">
+    </div>
+    
+    <div class="carousel-item">
+      <img src="https://cdn.pixabay.com/photo/2016/12/20/14/33/lobster-nebula-1920628__340.jpg" width="700" height="350" class="d-block w-200" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.pixabay.com/photo/2017/07/17/23/51/wormhole-2514312__340.jpg" width="700" height="350" class="d-block w-400" alt="..."> 
+    </div>
+  </div>
+  
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+ <!--  <script>
+   /*let main = document.querySelector("main");
+   for (node of main.childNodes) {
+     console.log(node)
+     console.log(main.parentNode)
+     console.log(main.parentElement)
+     console.log(document.body.parentNode)
+     console.log(document.body.parentElement)
+     
+   }*
+   
+   
+ </script> -->
+  
+  
+
+  </body>
+</html>
